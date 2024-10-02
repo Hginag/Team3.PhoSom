f24622265c39cf1667ecb19cf6efda0a424b876c
haowi1007 <hatr10704@gmail.com>
2024-10-02 23:50:36 +0700

    Add files via upload

---
+++ order.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Phở Tái </title>
+    <link rel="stylesheet" href="order.css">
+</head>
+<body>
+    <div class="product">
+        <img src = "photai.jpg" alt="Món 1">
+        <h1> Phở Tái</h1>
+        <p class="price">Giá: 60.000đ</p>
+        <div class="quantity">
+            <button>-</button>
+            <input type="text" value="1">
+            <button>+</button>
+        </div>
+        <button class="add-to-cart">Thêm vào giỏ hàng</button>
+        <button class="order-now">Đặt hàng</button>
+    </div>
+    <div class="product-info">
+        <h2>THÔNG TIN SẢN PHẨM</h2>
+        <!-- Thông tin sản phẩm -->
+         <p>Phở Tái Chín (Signature): Chín là phần thịt bò được luộc sẵn, thái bản to mỏng vừa miếng. Tái là phần thịt bò tươi thái lát nhỏ vừa miếng được đập dập dội nước dùng sôi lên thịt hoặc trần kĩ tuỳ khẩu vị khách hàng.</p>
+    </div>
+    <div class="related-products">
+            <h2>SẢN PHẨM CÙNG LOẠI</h2>
+        <!-- Sản phẩm cùng loại -->
+        <div class="product-card">
+        <img src="photai.jpg" alt="Món 2">
+                <h3>Món 2</h3>
+                <p>Giá bán: 60.000đ</p>
+        </div>
+    </div>
+</body>
+</html>
+
+
