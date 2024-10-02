57893074768f6adbbe09512bea072f46f1463f80
Hginag <phgiang@1389.com>
2024-10-02 22:25:11 +0700

    menu

---
+++ menu.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+    <link rel="stylesheet" href="menu.css">
+</head>
+<body style = "margin: 0;" >
+    <header>
+        <img class="banner" src="2.jpg" alt="Banner phở">
+    </header>
+    <main>
+    <div class="sidebar">
+        <a href="#pho">Phở</a>
+        <a href="#drinks">Drinks</a>
+        <a href="#snacks">Snacks</a>
+    </div>
+    <h3 id = "pho" class="name">PHỞ</h3>
+    <hr>
+    <div class="container">
+        <div class="menu-item">
+            <img src="1.png" alt="Món 1">
+            <h3>Phở tái</h3>
+        </div>
+        <div class="menu-item">
+            <img src="1.png" alt="Món 2">
+            <h3>Phở chín</h3>
+        </div>
+        <div class="menu-item">
+            <img src="1.png" alt="Món 4">
+            <h3>Phở nạm</h3>
+        </div>
+        <div class="menu-item">
+            <img src="1.png" alt="Món 5">
+            <h3>Phở bắp bò</h3>
+        </div>
+        <div class="menu-item">
+            <img src="1.png" alt="Món 6">
+            <h3>Phở gà</h3>
+        </div>
+        <div class="menu-item">
+            <img src="1.png" alt="Món 6">
+            <h3>Phở đặc biệt</h3>
+        </div>
+    </div>
+    <h3 id = "drinks" class="name">DRINKS</h3>
+    <hr>
+    <div class="container">
+        <div class="menu-item">
+            <img src="1.png" alt="Món 6">
+            <h3>Cafe đen</h3>
+        </div>
+        <div class="menu-item">
+            <img src="1.png" alt="Món 6">
+            <h3>Cafe sữa</h3>
+        </div>
+        <div class="menu-item">
+            <img src="1.png" alt="Món 6">
+            <h3>Bạc xỉu</h3>
+        </div>
+        <div class="menu-item">
+            <img src="1.png" alt="Món 6">
+            <h3>Trà chanh</h3>
+        </div>
+        <div class="menu-item">
+            <img src="1.png" alt="Món 6">
+            <h3>Trà quất</h3>
+        </div>
+        <div class="menu-item">
+            <img src="1.png" alt="Món 6">
+            <h3>Sữa đậu</h3>
+        </div>
+    </div>
+    <h3 id = "snacks" class="name">SNACKS</h3>
+    <hr>
+    <div class="container">   
+        <div class="menu-item">
+            <img src="1.png" alt="Món 6">
+            <h3>Hướng dương nguyên vị</h3>
+        </div>
+        <div class="menu-item">
+            <img src="1.png" alt="Món 6">
+            <h3>Hướng dương caramel</h3>
+        </div>
+        <div class="menu-item">
+            <img src="1.png" alt="Món 6">
+            <h3>Khô gà cay</h3>
+        </div>
+        <div class="menu-item">
+            <img src="1.png" alt="Món 6">
+            <h3>Khô bò cay</h3>
+        </div>
+        <div class="menu-item">
+            <img src="1.png" alt="Món 6">
+            <h3>Khô lợn cay</h3>
+        </div>
+        <div class="menu-item">
+            <img src="1.png" alt="Món 6">
+            <h3>Bánh su kem</h3>
+        </div>
+    </div>
+</body>
+</html>
