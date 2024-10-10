ebd324f61f420f2084a8220d35cbcc40d5081567
haowi1007 <hatr10704@gmail.com>
2024-10-11 00:02:30 +0700

    Update order.php

--- order.php
+++ order.php
@@ -3,30 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Phở Tái </title>
+    <title>Phở Tái</title>
     <link rel="stylesheet" href="order.css">
 </head>
 <body>
-    <div class="product">
-        <img src = "photai.jpg" alt="Món 1">
-        <h1> Phở Tái</h1>
-        <p class="price">Giá: 60.000đ</p>
-        <div class="quantity">
-            <button>-</button>
-            <input type="text" value="1">
-            <button>+</button>
+    <div class="product-container">
+        <img src="photai.jpg" alt="Món 1">
+        <div class="product-details">
+            <h1>PHỞ TÁI </h1>
+            <div class="price">60,000 VND</div>
+            <div class="quantity-control">
+                <button>-</button>
+                <input type="text" value="1">
+                <button>+</button>
+            </div>
+            <button class="add-to-cart">THÊM VÀO GIỎ HÀNG</button>
+            <div class="description">
+                <h2> THÔNG TIN SẢN PHẨM </h2>
+                <p>Phở Tái Chín (Signature): Chín là phần thịt bò được luộc sẵn, thái bản to mỏng vừa miếng. Tái là phần thịt bò tươi thái lát nhỏ vừa miếng được đập dập dội nước dùng sôi lên thịt hoặc trần kĩ tuỳ khẩu vị khách hàng.</p>
+            </div>
         </div>
-        <button class="add-to-cart">Thêm vào giỏ hàng</button>
-        <button class="order-now">Đặt hàng</button>
     </div>
-    <div class="product-info">
-        <h2>THÔNG TIN SẢN PHẨM</h2>
-        <!-- Thông tin sản phẩm -->
-         <p>Phở Tái Chín (Signature): Chín là phần thịt bò được luộc sẵn, thái bản to mỏng vừa miếng. Tái là phần thịt bò tươi thái lát nhỏ vừa miếng được đập dập dội nước dùng sôi lên thịt hoặc trần kĩ tuỳ khẩu vị khách hàng.</p>
-    </div>
+    <hr>
+    <div class="container">
     <div class="related-products">
             <h2>SẢN PHẨM CÙNG LOẠI</h2>
-        <!-- Sản phẩm cùng loại -->
         <div class="product-card">
         <img src="photai.jpg" alt="Món 2">
                 <h3>Món 2</h3>
@@ -37,8 +38,17 @@
                 <h3>Món 3</h3>
                 <p>Giá bán: 60.000đ</p>
         </div>
+        <div class="product-card">
+        <img src="photai.jpg" alt="Món 4">
+                <h3>Món 4</h3>
+                <p>Giá bán: 60.000đ</p>
+        </div>
+        <div class="product-card">
+        <img src="photai.jpg" alt="Món 5">
+                <h3>Món 5</h3>
+                <p>Giá bán: 60.000đ</p>
+        </div>
     </div>
+</div>
 </body>
 </html>
-
-
