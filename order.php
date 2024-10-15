93a86dd63005f4cf02a7d858945753930855a431
haowi1007 <hatr10704@gmail.com>
2024-10-15 21:42:14 +0700

    Update order.php

--- order.php
+++ order.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="vi">
 <head>
@@ -7,6 +8,24 @@
     <link rel="stylesheet" href="order.css">
 </head>
 <body>
+<header class="header">
+        <div class="logo">
+            <img src="logo.png" alt="Logo">
+            <div class="name">
+                <h1><b>PHỞ SỚM</b></h1>
+                <h2 class="slogan"><i>Thơm lừng, ngon tuyệt</i></h2>
+            </div>
+        </div>
+        <nav class="nav">
+            <a href="">Trang Chủ</a>
+            <a href="">Giới Thiệu</a>
+            <a href="">Đặt Bàn</a>
+            <a href="">Đặt Món</a>
+            <a href="">Giỏ Hàng</a>
+            <a href="">Đăng Nhập</a>
+        </nav>
+    </header>
+    
     <div class="product-container">
         <img src="photai1.jpg" alt="Món 1">
         <div class="product-details">
@@ -50,5 +69,36 @@
             <img src="pho.jpg" alt="Món 7">
             <h3>Phở đặc biệt</h3>
         </div>
+        <footer>
+        <div class="footer-content">
+            <div class="footer-center">
+            <img src="logo.png" alt="Logo" class="footer-logo">
+            <h2>PHỞ SỚM</h2>
+            <p><i>Thơm lừng, ngon tuyệt</i></p>
+            </div>
+            <div class="footer-line">
+            <div class="footer-left">
+                <ul>
+                <li><a href="">Trang Chủ</a></li>
+                <li><a href="">Giới Thiệu</a></li>
+                <li><a href="">Đặt Bàn</a></li>
+                <li><a href="">Đặt Món</a></li>
+                </ul>
+            </div>
+            <div class="footer-right">
+                <ul>
+                <li><h3><b>Chi Nhánh Hà Nội</b></h3>
+                    <i> Địa chỉ: Số 23 Hồ Tùng Mậu, P. Mai Dịch, Q. Cầu Giấy, TP. Hà Nội 
+                        <br> Điện thoại: 0922 266 966</i></li>
+                </ul>
+            </div>
+            </div>
+            <div class="footer-social">
+            <a href="https://facebook.com"><img src="https://img.icons8.com/?size=100&id=118467&format=png&color=FFFFFF" alt="Facebook"></a>
+            <a href="https://instagram.com"><img src="https://img.icons8.com/?size=100&id=32309&format=png&color=FFFFFF" alt="Instagram"></a>
+            <a href="https://instagram.com"><img src="https://img.icons8.com/?size=100&id=53439&format=png&color=FFFFFF" alt="Instagram"></a>
+            </div>
+        </div>
+        </footer>
 </body>
 </html>
