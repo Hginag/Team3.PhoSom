226bf71a9ba79ae1606bf48da75b7a25d4ee76ef
NguyenBaongoc1310 <nnguyenbaongoc03@gmail.com>
2024-10-15 20:49:20 +0700

    Update and rename aboutus.html to aboutus.php

---
+++ aboutus.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link href="about.css"
+	rel="stylesheet"integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl"
+	crossorigin="anonymous"/>
+	<link rel="stylesheet" href="about.css" />
+	<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap"rel="stylesheet"/>
+	<title> Giới thiệu</title>
+</head>
+<body>
+	<!--Header-->
+		<?php
+	        require "Header.php";
+	    	?>
+	<!-- Banner -->
+	<section id="banner">
+		<div class="container-fluid" id="banner-container">
+			<div class="row" id="banner-row">
+				<div class="col-md-7" id="banner-col">
+					<h3>Giới thiệu</h3>
+					<p><I>Ở Hà Nội, phở là một món ăn đặc biệt. Phở được dùng riêng như là một món ăn sáng, trưa hoặc tối. Nước dùng của phở được làm từ nước ninh của xương bò: xương cục, xương ống và xương vè. Thịt dùng cho món phở có thể là bò, hoặc gà. Bánh phở phải mỏng và dai mềm, gia vị của phở là hành lá, hạt tiêu, giấm ớt, lát chanh thái </I> </p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<hr/>
+
+	<!-- Thương hiệu -->
+	<section id="history">
+		<h2>Thương hiệu Phở sớm</h2>
+		<div class="history-nd">
+			<P><span class="first-letter">Đ</span>ã từng có người ví Phở như một trường phái nghệ thuật đặc biệt trong lĩnh vực ẩm thực và người làm phở như một nghệ nhân thực thụ. Nghệ thuật ở đây là nghệ thuật chế biến, nghệ thuật gửi gắm cảm xúc, tâm huyết của người làm trong từng tô Phở. Nghệ thuật của sự đưa hương tài tình và đầy quyến rũ. Không chỉ nổi tiếng trong nước mà Phở Sớm còn được bạn bè nước ngoài biết đến. Những du khách nước ngoài đến Việt Nam đã từng nghe về đặc sản Phở hẳn họ sẽ tìm đến thương hiệu Phở Sớm. Được người dân trong nước yêu mến, bạn bè quốc tế công nhận, Phở Sớm thực sự đã có một vị trí quan trọng trong lòng thực khách, cũng như đưa ẩm thực Việt vươn ra thế giới lên một tầm cao mới.
+			Với nhu cầu của người Việt về các sản phẩm ăn uống, thì mô hình kinh doanh Phở Sớm hứa hẹn sẽ là sản phẩm mang lại cơ hội đầu tư an toàn, chắc lãi,giảm thiểu rủi ro, nắm chắc thành công. Đặc biệt, khi kết hợp với Công ty Đặc sản Quốc gia nhà đầu tư sẽ được tiếp nhận công nghệ kinh doanh với mô hình chuẩn nhất, được đào tạo chuyên nghiệp về kỹ năng vận hành quản lý,  được chuyển giao công thức nấu ăn hấp dẫn thực khách, cung cấp bộ hồ sơ đã áp dụng hàng trăm quán trên toàn quốc, giải quyết bài toán quan trọng nhất ngay từ khi bắt đầu kinh doanh: khách hàng, được đào tạo,dạy cách có khách thông qua các chương trình marketing bài bản, hiệu quả,…đảm bảo cho nhà đầu tư mở đâu thắng đó.
+			<br><br><strong>Tầm Nhìn</strong>
+				<br><p><span class="first-letter">P</span>hở Sớm không chỉ là nơi để thưởng thức một tô phở ngon miệng, mà còn là một điểm hẹn lưu giữ những khoảnh khắc đẹp, nơi mọi người cùng nhau chia sẻ niềm vui và sự ấm áp ngay từ sớm bình minh</p>
+
+			<br><strong>Sứ mệnh</strong>
+				<OL type="1">
+					<LI>Mang đến cho khách hàng hương vị phở truyền thống,được chế biến từ những nguyên liệu tươi ngon nhất </lI>
+					<LI>Tạo nên một không gian thoải mái và thân thiện, nơi mọi người tụ họp và bắt đầu ngày mới với nguồnnăng lượng tích cực</LI>
+					<li>Gìn giữ và phát huy giá trị của món phở Việt Nam, đồng thời không ngừng sáng tạo và đổi mới đem lạihương vị mới lạ, phù hợp với khẩu vị đa dạng củatừng khách hàng khác nhau</li>
+				</OL>
+
+			<br><strong>Slogan</strong>
+				<br><P><span class="first-letter">P</span>hở Sớm - Thơm lừng, ngon tuyệt</P>
+			
+		</div>
+		<img src="./imagine/picture2.jpg" class="history-image">
+	</section>
+
+	<!--Nhân viên -->
+	<section id="employee">
+		<h2>Đội ngũ nhân viên</h2>
+		<div class="owl-carousel works__img">
+			<div class="item">
+				<img src="./imagine/cheef1.jpg" >
+				<p><strong>Bếp chính Phan Tôn Tịnh Hải </strong>
+					<br>Bà đã có hơn 20 năm kinh nghiệm trong ngành ẩm thực và tốt nghiệp Thạc sĩ ngành Ẩm thực và Dinh dưỡng tại New York
+				</p>
+			</div>
+			<div class="item">
+				<img src="./imagine/cheef2.jpg" >
+				<p><strong>Bếp chính Siu Hin-Chi</strong>
+					<br>Đầu bếp Siu có hơn 37 năm kinh nghiệm trong ngành ẩm thực và từng đảm nhiệm vai trò bếp trưởng của nhiều nhà hàng sao Michelin ở Hồng Kông như Tang Court, Duddell’s
+			</div>
+			<div class="item">
+				<img src="./imagine/cheef3.jpg" >
+				<p><strong>Bếp phụ Thanh Hòa</strong>
+					<br>Hơn 10 năm ở vai trò bếp trưởng, Vua Đầu bếp Việt mùa 1 (2013)
+			</div>
+			<div class="item">
+				<img src="./imagine/cheef4.jpg">
+				<p><strong>Bếp phụ Sam Trần</strong>
+					<br>Nữ đầu bếp đầu tiên của Việt Nam giành một sao Michelin cho nhà hàng Gia
+			</div> 
+		</div>
+	</section>
+
+	<!--Footer-->
+		<?php
+	        require "Footer.php";
+	    	?>
+	
+</body>
+</html>
